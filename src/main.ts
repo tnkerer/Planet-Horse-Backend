ce521fb94efc152d704b201bfe2b5501c80964fa
zyantes <phillipe.o.m@hotmail.com>
2025-06-02 19:48:21 -0300

    new migration strategy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
+import { PrismaClient } from '@prisma/client';
 require('dotenv').config();
 
 async function bootstrap() {
+  const prisma = new PrismaClient();
+
+  try {
+    await prisma.$executeRawUnsafe(`SELECT 1`);
+    await prisma.$disconnect();
+
+    // Run migrations (optional if schema is already synced)
+    const { exec } = await import('child_process');
+    exec('npx prisma migrate deploy', (err, stdout, stderr) => {
+      if (err) {
+        console.error('Migration error:', stderr);
+      } else {
+        console.log('Migration result:', stdout);
+      }
+    });
+  } catch (error) {
+    console.error('Could not connect to the database.', error);
+  }
+  
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
