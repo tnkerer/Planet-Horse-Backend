cad34e9503a16667b09f8d4007219e4c24b26ed5
zyantes <phillipe.o.m@hotmail.com>
2025-06-03 00:57:08 -0300

    fix: prisma migration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
+import { PrismaClient } from '@prisma/client';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 require('dotenv').config();
 
+const asyncExec = promisify(exec);
+
 async function bootstrap() {
+  const prisma = new PrismaClient();
+
+try {
+    // Check DB connection
+    await prisma.$queryRaw`SELECT 1`;
+
+    console.log('Connected to database. Running Prisma migration...');
+    const { stdout, stderr } = await asyncExec('npx prisma migrate deploy');
+
+    if (stderr) {
+      console.error('Migration stderr:', stderr);
+    } else {
+      console.log('Migration stdout:', stdout);
+    }
+  } catch (err) {
+    console.error('Error connecting to database or running migration:', err);
+  } finally {
+    await prisma.$disconnect();
+  }
+
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
